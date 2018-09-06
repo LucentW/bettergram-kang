3c3ec3900ef47a8ff23a3f5d739c27015ffab27d
Gilmanov Ildar <dev@ddwarf.org>
2018-09-06 23:07:56 +0700

    Customize text strings for videos tab

--- Telegram/SourceFiles/chat_helpers/videos_widget.cpp
+++ Telegram/SourceFiles/chat_helpers/videos_widget.cpp
@@ -3,6 +3,7 @@
 #include <bettergram/bettergramservice.h>
 
 #include <styles/style_chat_helpers.h>
+#include <lang/lang_keys.h>
 
 namespace ChatHelpers {
 
@@ -12,6 +13,11 @@ VideosWidget::VideosWidget(QWidget* parent, not_null<Window::Controller*> contro
 	: RssWidget(parent,
 				controller,
 				BettergramService::instance()->videoChannelList(),
+				lang(lng_videos_show_only_unread_videos),
+				lang(lng_videos_show_all_videos),
+				lang(lng_menu_videos_mark_as_watched),
+				lang(lng_menu_videos_mark_all_site_videos_as_watched),
+				lang(lng_menu_videos_mark_all_videos_as_watched),
 				st::videosPanNewsReadFg,
 				st::videosPanNewsBodyFg,
 				st::videosPanNewsHeaderFg,
